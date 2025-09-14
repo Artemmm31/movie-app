9c001596a3e09d7c723611f75e7e40953749f975
name <you@example.com>
2025-09-14 15:47:46 +0300

    feat: navigation bar

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Saved() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500 font-bold">Home</Text>
+      <Text className="text-5xl text-blue-500 font-bold">Saved</Text>
     </View>
   );
 }
